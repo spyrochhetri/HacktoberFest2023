4ca78e527a6a660822ffd8bc2c1188281dc1f1a0
Subash Paudel Chhetri <69474140+spyrochhetri@users.noreply.github.com>
2023-10-17 13:58:25 -0400

    Create binarysearch.py
    This code defines a binary_search function that takes a sorted array (arr) and a target value as input. It returns the index of the target in the array if found or -1 if the target is not in the array. The binary search algorithm works by repeatedly dividing the search range in half until the target is found or it's determined that the target is not in the array.

---
+++ binarysearch.py
@@ -0,0 +1,23 @@
+def binary_search(arr, target):
+    left, right = 0, len(arr) - 1
+
+    while left <= right:
+        mid = left + (right - left) // 2  # Calculate the middle index
+
+        if arr[mid] == target:
+            return mid  # Target found, return the index
+        elif arr[mid] < target:
+            left = mid + 1  # Adjust the left bound
+        else:
+            right = mid - 1  # Adjust the right bound
+
+    return -1  # Target not found in the array
+
+# Example usage:
+arr = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
+target = 7
+result = binary_search(arr, target)
+if result != -1:
+    print(f"Element {target} found at index {result}")
+else:
+    print(f"Element {target} not found in the array")
